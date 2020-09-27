a0ba589e25b4a648903eb1988980f12414b9a24c
smshack <5432tat@naver.com>
2020-09-28 03:37:01 +0900

    강의상자 기능 추가

--- front/main.js
+++ front/main.js
@@ -5,15 +5,18 @@ function loadItems() {
     .then((json) => json.items);
 }
 
+const login_form = document.querySelector('.login_form');
+const list_items = document.querySelector('.items');
 // Create HTML list item from the given
 function createHTMLString(item) {
   const baseUrl = 'https://smshack.github.io/front/';
   return `
     <li>
-    <a
+    <a class="view"
       href="${baseUrl}${item.url}"
       >로그인폼 ${item.num}</a
     >  <a href="${item.link}">강의링크</a>
+    <button class="pre_view">미리보기</button>
   </li>`;
 }
 
@@ -21,14 +24,39 @@ function displayItems(items) {
   const container = document.querySelector('.items');
   const html = items.map((item) => createHTMLString(item)).join('');
   container.innerHTML = items.map((item) => createHTMLString(item)).join('');
-  console.log(html);
+  //console.log(html);
 }
 
 //main
-loadItems()
-  .then((items) => {
-    console.log(items);
-    displayItems(items);
-    setEventListeners(items);
-  })
-  .catch(console.log);
+
+login_form.addEventListener('click', () => {
+  loadItems()
+    .then((items) => {
+      console.log(items);
+      displayItems(items);
+
+      //setEventListeners(items);
+    })
+    .catch(console.log);
+});
+async function getBanana() {
+  await delay(500);
+  return '바나나';
+}
+
+//------------------------------------------------------------------
+list_items.addEventListener('click', (event) => {
+  if (event.target.tagName === 'BUTTON') {
+    const view = event.target.parentNode.querySelector('.view')['href'];
+    console.log(view);
+    createLecContainer(view);
+  }
+});
+
+function createLecContainer(view) {
+  const lec_container = document.querySelector('.lec_container');
+  lec_container.innerHTML = `
+  <iframe
+    src="${view}"
+  ></iframe>`;
+}
